a9f91569778e0c129e18ab7a4efd90157c7e5c94
MofiqueKhan <khanmofique@gmail.com>
2024-04-26 00:46:24 +0530

    creating & setup endpoints and testing API's from Postman

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("hello");
+// console.log("hello");
+
+const express = require("express");
+const app = express();
+const quotes = require("./quotes.json")
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.get("/quote" , (req , res)=>{
+    res.status(200).json(quotes)
+})
+
+app.get("/random" , (req , res)=>{
+  let index = Math.floor(Math.random() * quotes.length);
+  let quote = quotes[index];
+  res.status(200).json(quote);
+})
+
+app.listen(5000, () => {
+  console.log("server started on port : 5000");
+});
